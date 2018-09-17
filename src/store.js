599f328704720426d00a132b06fd0746fa18cbdf
Rafael Santos <rafael.santos.193@gmail.com>
2018-09-17 05:54:12 -0300

    Instalados pacotes

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/root-reducer';
+import { createWebsocketMiddleware } from "redux-websocket-middleware"
+
+const socketMiddleware = createWebsocketMiddleware({
+});
 
 export default () => createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, socketMiddleware)
 );
