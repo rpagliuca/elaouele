8b6d457265f04dc2c0b56a9e8a490e755692c1b5
Rafael Santos <rafael.santos.193@gmail.com>
2018-09-17 07:35:07 -0300

    Uninstall unused package

--- src/store.js
+++ src/store.js
@@ -1,12 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/root-reducer';
-import { createWebsocketMiddleware } from "redux-websocket-middleware"
-
-const socketMiddleware = createWebsocketMiddleware({
-});
 
 export default () => createStore(
     rootReducer,
-    applyMiddleware(thunk, socketMiddleware)
+    applyMiddleware(thunk)
 );
