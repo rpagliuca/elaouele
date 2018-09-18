7e9a53144ca965bf56b7cae6edb5ce8477cb2521
Rafael Santos <rafael.santos.193@gmail.com>
2018-09-17 21:09:33 -0300

    fix: Fix store initialization

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ import rootReducer from './reducers/root-reducer';
 
 export default () => createStore(
     rootReducer,
+    {},
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
